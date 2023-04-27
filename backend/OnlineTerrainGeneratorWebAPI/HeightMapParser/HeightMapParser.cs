8e278bbd6c48c2d1cbe714bf8d0bd05351b539fe
iamshatskiy <81781208+iamshatskiy@users.noreply.github.com>
2023-04-28 00:38:58 +0300

    Create HeightMapParser.cs

---
+++ backend/OnlineTerrainGeneratorWebAPI/HeightMapParser/HeightMapParser.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using Newtonsoft.Json.Linq;
+
+namespace OnlineTerrainGeneratorWebAPI.HeightMapParser
+{
+    
+
+    public static class HeightMapParser
+    {
+        public struct HeigthMapParameters
+        {
+            public string func;
+            public string alg;
+            public float[] options;
+        };
+        
+
+        public static HeigthMapParameters JsonParser(string jsonString)
+        {
+            HeigthMapParameters parameters;
+            JObject jsonObject = JObject.Parse(jsonString);
+
+            string func = (string)jsonObject["func"];
+            string alg = (string)jsonObject["alg"];
+            JArray options = (JArray)jsonObject["options"];
+
+            float[] optionsArray = options.ToObject<float[]>();
+
+            parameters.func = func;
+            parameters.alg = alg;
+            parameters.options = optionsArray;
+
+            return parameters;
+        }
+
+        public static Func<double, double> HeigthMapFunction(string expression)
+        {
+            var x = Expression.Parameter(typeof(double), "x");
+            var lambda = DynamicExpressionParser.ParseLambda(new[] { x }, null, expression);
+
+            return (Func<double, double>)lambda.Compile();
+        }
+    }
+}
