296b7840a91e93d3cc14211d5f71b72cd28b6b74
iamshatskiy <81781208+iamshatskiy@users.noreply.github.com>
2023-04-28 21:19:50 +0300

    Update HeightMapLogic.cs

--- backend/OnlineTerrainGeneratorWebAPI/HeightMapLogic/HeightMapLogic.cs
+++ backend/OnlineTerrainGeneratorWebAPI/HeightMapLogic/HeightMapLogic.cs
@@ -1,4 +1,5 @@
-﻿using SixLabors.ImageSharp;
+﻿using Microsoft.AspNetCore.Routing;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using TerrainGenerator;
 using static OnlineTerrainGeneratorWebAPI.HeightMapParser.HeightMapParser;
@@ -9,31 +10,43 @@ namespace OnlineTerrainGeneratorWebAPI.HeightMapLogic
     {
         HeightMap _heightMap;
 
-        private const int Size = 512;
+        private const int Size = 513;
 
+        private static ILandGenerator CreateLandGenerator(GenerationAlgoritm algoritm, float[] optns)
+        {
+            ILandGenerator landGenerator = null;
+            switch (algoritm)
+            {
+                case GenerationAlgoritm.DiamondSquare:
+                    landGenerator = new DiamondSquare(optns[0], (int?)optns[1]);
+                    break;
+                case GenerationAlgoritm.PerlinNoise:
+                    landGenerator = new PerlinNoise((int)optns[0], (int)optns[1], optns[2], optns[3], (int?)optns[4]);
+                    break;
+                case GenerationAlgoritm.SimplexNoise:
+                    landGenerator = new SimplexNoise((int)optns[0], (int)optns[1], optns[2], optns[3], (int?)optns[4]);
+                    break;
+            }
+            return landGenerator;
+        }
         public HeightMapLogic(string JsonString)
         {
             var parameters = JsonParser(JsonString);
             var optns = parameters.AlgorithmParams;
-            ILandGenerator landGenerator;
+            ILandGenerator landGenerator = CreateLandGenerator(parameters.Algorithm, parameters.AlgorithmParams);
 
-            switch (parameters.Algorithm)
-            {
-                case GenerationAlgoritm.DiamondSquare:
-                    landGenerator = new DiamondSquare(optns[0], (int?)optns[1]);
-                    _heightMap = new HeightMap(Size + 1, Size + 1, landGenerator, new NoiseExpresion(parameters.NoiseExpression));
-                    break;
-                case GenerationAlgoritm.PerlinNoise:
-                    landGenerator = new PerlinNoise((int)optns[0], (int)optns[1], optns[2], optns[3], (int?)optns[4]);
-                    _heightMap = new HeightMap(Size, Size, landGenerator, new NoiseExpresion(parameters.NoiseExpression));
-                    break;
-                case GenerationAlgoritm.SimplexNoise:
-                    landGenerator = new SimplexNoise((int)optns[0], (int)optns[1], optns[2], optns[3], (int?)optns[4]);
-                    _heightMap = new HeightMap(Size, Size, landGenerator, new NoiseExpresion(parameters.NoiseExpression));
-                    break;
-                default:
-                    break;
-            }
+            _heightMap = new HeightMap(Size, Size, landGenerator, new NoiseExpresion(parameters.NoiseExpression));
+
         }
+
+        public void UpdateHeightMap(string JsonString)
+        {
+            var parameters = JsonParser(JsonString);
+            var optns = parameters.AlgorithmParams;
+            _heightMap.LandGenerator = CreateLandGenerator(parameters.Algorithm, parameters.AlgorithmParams);
+            _heightMap.NoiseExpression = new NoiseExpresion(parameters.NoiseExpression);
+        }
+
+        
     }
 }
