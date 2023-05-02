0d98bb07edddfbe881aab0d13130b635e7e088fa
Daniel Ageev <daniel.ageev0000@mail.ru>
2023-05-02 23:36:52 +0300

    add swagger comments

--- backend/OnlineTerrainGeneratorWebAPI/HeightMapLogic/UrlCreator.cs
+++ backend/OnlineTerrainGeneratorWebAPI/HeightMapLogic/UrlCreator.cs
@@ -3,15 +3,31 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace OnlineTerrainGeneratorWebAPI.Logic
 {
+    /// <summary>
+    /// Класс, отвечающий за создание URL
+    /// </summary>
     public class UrlCreator
     {
+        /// <summary>
+        /// Среда размещения
+        /// </summary>
         readonly IWebHostEnvironment _hostingEnvironment;
 
+        /// <summary>
+        /// Инициализация класса, отвечающего за создание URL
+        /// </summary>
+        /// <param name="hostingEnvironment">Среда размещения</param>
         public UrlCreator(IWebHostEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
         }
-
+        /// <summary>
+        /// Создает URL изображения
+        /// </summary>
+        /// <param name="request">HTTP запрос</param>
+        /// <param name="image">Изображение</param>
+        /// <param name="filename">Имя файла</param>
+        /// <returns>URL сохраненного изображенния</returns>
         public string CreateImageUrl(HttpRequest request, Image<Rgba32> image, string filename)
         {
             var uploadFolder = Path.Combine(_hostingEnvironment.ContentRootPath, "wwwroot/images");
